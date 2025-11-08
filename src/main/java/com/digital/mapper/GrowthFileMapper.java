73a336e25877cbab9660846c6c0c84e4c2b9fc79
zhaoshuang <1554096735@qq.com>
2025-11-08 09:16:34 +0800

    feat: add GrowthFile class for managing growth record files

---
+++ src/main/java/com/digital/mapper/GrowthFileMapper.java
@@ -0,0 +1,14 @@
+package com.digital.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.digital.model.entity.GrowthFile;
+
+/**
+ * 成长记录文件数据库操作
+ *
+ * @author Shane
+ */
+public interface GrowthFileMapper extends BaseMapper<GrowthFile> {
+
+}
+
